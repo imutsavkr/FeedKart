13407e71ec2cfee32051075befca23ca2b9643bc
Utsav Kumar <45901934+Utsav418@users.noreply.github.com>
2022-03-15 13:05:52 +0530

    App Name changed to feedkart from news-App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    c = 'utsav'
+    render() {
+        return (
+            <div>
+                Hello my first class based component {this.c}
+            </div>
+        )
+    }
+}
+
 
 
 
