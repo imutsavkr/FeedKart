a7e14dd1c4f3252bfffcb01996f7b6d4224c9781
Utsav Kumar <45901934+Utsav418@users.noreply.github.com>
2022-03-16 18:54:19 +0530

    FeedKart Updated

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import loading from './loading.gif'
 
 export class Spinner extends Component {
-
     render() {
         return (
             <div className="text-center">
