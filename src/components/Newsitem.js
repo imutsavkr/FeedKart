ba39a8bf761e5d133fc87a4b3cf7dbd7e516fdf4
Utsav Kumar <45901934+Utsav418@users.noreply.github.com>
2022-03-16 18:46:48 +0530

    used props to categorise news

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -6,11 +6,11 @@ export class NewsItem extends Component {
         return (
             <div className="my-3">
                 <div className="card" style={{ width: "18rem" }}>
-                    <img src={imageUrl} className="card-img-top" alt="..." />
+                    <img src={!imageUrl?"https://a57.foxnews.com/static.foxbusiness.com/foxbusiness.com/content/uploads/2022/03/0/0/Starbucks-Reusable-4A.jpg?ve=1&tl=1":imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">Read More</a>
+                        <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
